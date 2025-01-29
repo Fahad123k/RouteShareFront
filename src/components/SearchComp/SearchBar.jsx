0d254518dba39bafeaffc064e59571496942d0e7
fahad123k <ahmad.nishu08@gmail.com>
2025-01-29 18:51:27 +0530

    #implement static search

--- src/components/SearchComp/SearchBar.jsx
+++ src/components/SearchComp/SearchBar.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { use } from 'react';
 import { FaRegCircleDot } from "react-icons/fa6";
 import { HiOutlineCalendarDateRange } from "react-icons/hi2";
 import { RiWeightLine } from "react-icons/ri";
-import { NavLink } from "react-router";
+import { NavLink ,useNavigate} from "react-router";
 
 const SearchBar = () => {
+
+  const [leaveFrom,setLeaveFrom]=useState('')
+  const [goingTo,setGoinTo]=useState('')
+  const [date,setDate]=useState('')
+  const [size,setSize]=useState('M')
+
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    console.log("leve from:",leaveFrom,","," goingTo",goingTo," Date :","date"," Size :",size)
+    // navigate(`/search?leaveFrom=${leaveFrom}&goingTo=${goingTo}&date=${date}&size=${size}`);
+
+  }
   return (
-    <div className=" w-full max-w-7xl  flex justify-center items-center m-auto ">
+    <div className=" w-full max-w-7xl  flex justify-center items-center m-auto "
+    onSubmit={handleSubmit}
+    >
       <form className="flex flex-col lg:flex-row p-2 rounded-md shadow-md items-center justify-center gap-4 w-full  bg-white border border-gray-200">
         <div className="flex items-center border-b border-gray-200  w-full sm:w-1/2">
           <span className="inline-flex items-center px-3  text-sm text-gray-900  rounded-l-md ">
@@ -16,6 +32,8 @@ const SearchBar = () => {
             type="text"
             className="rounded-r-md  text-gray-900 block w-full text-sm p-2.5   "
             placeholder="Leave from"
+            value={leaveFrom}
+            onChange={(e)=>setLeaveFrom(e.target.value)}
           />
         </div>
 
@@ -28,6 +46,8 @@ const SearchBar = () => {
             type="text"
             className="rounded-r-md  text-gray-900 block w-full text-sm p-2.5   "
             placeholder="Going to"
+            value={goingTo}
+            onChange={(e)=>setGoinTo(e.target.value)}
           />
         </div>
 
@@ -39,9 +59,10 @@ const SearchBar = () => {
           <input
             type="date"
             className="rounded-r-md text-gray-900 block w-full text-sm p-2.5"
-            onFocus={(e) => (e.target.type = "date")}
-            onBlur={(e) => (e.target.value === "" ? (e.target.type = "text") : null)}
+            
             placeholder="Date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
           />
 
         </div>
@@ -53,24 +74,26 @@ const SearchBar = () => {
           </span>
           <select
             className="rounded-r-md text-gray-900 block w-full text-sm p-2.5 bg-white  focus:ring-blue-500 focus:border-blue-500"
+            value={size}
+            onChange={(e)=>setSize(e.target.value)}
           >
-            <option value="extra-small">Extra Small</option>
-            <option value="small">Small</option>
-            <option value="medium">Medium</option>
-            <option value="large">Large</option>
+            <option value="XM">Extra Small</option>
+            <option value="S">Small</option>
+            <option value="M">Medium</option>
+            <option value="L">Large</option>
           </select>
         </div>
 
         <div className="float-end w-full sm:w-1/2">
-          <NavLink to='/search'>
+          {/* <NavLink to='/search'> */}
 
             <input
-              type="button"
+              type="submit"
               className="rounded-b-lg  md:rounded-r-lg block w-full text-white text-sm p-2.5 bg-black  "
 
               value="Search"
             />
-          </NavLink>
+          {/* </NavLink> */}
         </div>
       </form>
     </div>
