1d80bea08771271c0ac35ca70d8c89b09dc42e0c
fahad123k <ahmad.nishu08@gmail.com>
2025-02-19 19:16:50 +0530

    correct username

--- src/Pages/AllJourney.jsx
+++ src/Pages/AllJourney.jsx
@@ -24,16 +24,14 @@ const SkeletonLoader = ({ rows = 5, height = "150px", width = "100%" }) => {
                         <div className="flex-1 flex items-center justify-between ">
                             <div className="flex-col w-full">
 
-                                {/* <p className="text-gray-500">{travel.departureTime}</p> */}
-                                {/* <p className="font-bold text-gray-800">Delhi</p> */}
-                                {/* <p className="font-bold text-gray-800">{travel.leaveFrom.city}</p> */}
+
                             </div>
                             <span className="w-full flex items-center m-2 ">
 
 
                                 <div className="line h-1 w-full bg-gray-200 "></div>
                             </span>
-                            {/* <p>0h30</p> */}
+
                             <span className="w-full flex items-center m-2 ">
 
                                 <div className="line h-1 w-full bg-gray-200 "></div>
@@ -41,14 +39,11 @@ const SkeletonLoader = ({ rows = 5, height = "150px", width = "100%" }) => {
                             </span>
                             <div className="flex-col">
 
-                                {/* <p className="text-gray-500">{travel.arrivalTime}</p>
-                                {/* <p className="font-bold text-gray-800">Gurgaoun</p> */}
-                                {/* <p className="font-bold text-gray-800">{travel.goingTo.city}</p> */}
+
                             </div>
 
                         </div>
                         <div className="flex-1 text-right">
-                            {/* <p className="text-xl font-bold text-gray-800">{travel.fareStart}</p> */}
                         </div>
                     </div>
 
@@ -60,23 +55,17 @@ const SkeletonLoader = ({ rows = 5, height = "150px", width = "100%" }) => {
                             <div className="flex-col mr-2">
 
 
-                                {/* <p className="text-gray-800">{travel.departureTime}</p>
-                                <p className="text-gray-400 text-sm">0h30</p>
-                                <p className="text-gray-800">{travel.arrivalTime}</p> */}
+
                             </div>
                             <div className="line m-2">
 
                             </div>
 
                             <div className="flex-col">
-
-                                {/* <p className="font-bold text-gray-700">{travel.goingTo.lat}</p> */}
-                                {/* <p className="font-bold text-gray-700">{travel.leaveFrom.lat}</p> */}
                             </div>
 
                         </div>
                         <div className="flex-1 text-right">
-                            {/* <p className="text-xl font-bold text-gray-800">{travel.fareStart}</p> */}
                         </div>
                     </div>
 
@@ -230,7 +219,7 @@ const AllJourney = () => {
                                 <LiaCarSideSolid className="text-3xl" />
                                 <p className="text-gray-600"> {travel.departureTime}</p>
                                 <BiUserCircle className="text-3xl" />
-                                <p className="text-gray-600">{travel.userId.substring(0, 5)}</p>
+                                <p className="text-gray-600">{travel.username.substring(0, 10)}</p>
                                 <p className="text-gray-600">Capacity:{travel.maxCapacity}</p>
                                 <p className="text-gray-600 flex items-center">
                                     <IoStarSharp className="mr-2" /> 4.4
